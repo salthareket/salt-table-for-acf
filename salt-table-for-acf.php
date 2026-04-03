96835f17b378cc9c1928a29c30e6f9a01d6496b7
salthareket <info@salthareket.com>
2026-04-03 12:34:16 +0300

    Remove 'Requires Plugins' header from plugin
    Delete the `Requires Plugins: advanced-custom-fields` line from the plugin header in salt-table-for-acf.php. This removes an explicit dependency declaration from the plugin metadata; no functional code changes were made.

--- salt-table-for-acf.php
+++ salt-table-for-acf.php
@@ -10,7 +10,6 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: salt-table-for-acf
  * Domain Path: /languages
- * Requires Plugins: advanced-custom-fields
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
